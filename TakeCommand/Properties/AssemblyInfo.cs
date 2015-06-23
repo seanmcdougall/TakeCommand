9acfb48bd001e3cd286d1c0260e9ef2e51f9f24a
spm <seanmcdougall@users.noreply.github.com>
2015-06-22 23:22:51 -0300

    Recompile for 1.0.3

--- TakeCommand/Properties/AssemblyInfo.cs
+++ TakeCommand/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("TakeCommand")]
 [assembly: AssemblyDescription("Allows Kerbals to be launched in External Command Seats")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Sean McDOugall")]
+[assembly: AssemblyCompany("Sean McDougall")]
 [assembly: AssemblyProduct("TakeCommand")]
 [assembly: AssemblyCopyright("Copyright © Sean McDougall 2015")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.3.0")]
-[assembly: AssemblyFileVersion("1.1.3.0")]
+[assembly: AssemblyVersion("1.1.4.0")]
+[assembly: AssemblyFileVersion("1.1.4.0")]
